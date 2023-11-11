070a7d259ae171e3c9ce210c7ead54cc01031f45
Opeyemi Odunayo <opeyemiodunayo2018@gmail.com>
2023-11-11 16:58:42 +0100

    i editd a code

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 let faMoon = document.querySelector(".fa-moon");
 let faSun = document.querySelector(".fa-sun");
-let mainContainer = document.querySelector(".main-container");
+let bodyElment = document.body
+let form = document.getElementById("form")
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme) {
-  mainContainer.classList.add(savedTheme);
+  bodyElment.classList.add(savedTheme);
   if (savedTheme === "dark-theme") {
     faMoon.classList.replace("fa-moon", "fa-sun");
   }
 }
 
 faMoon.addEventListener("click", () => {
-  mainContainer.classList.toggle("dark-theme");
+  bodyElment.classList.toggle("dark-theme");
 
   if (faMoon.classList.contains("fa-moon")) {
     faMoon.classList.replace("fa-moon", "fa-sun");
@@ -40,73 +41,97 @@ if (savedDictionaryData) {
   dictionaryDiv.innerHTML = savedDictionaryData;
 }
 
-searchIcon.addEventListener("click", () => {
+searchIcon.addEventListener("click", ()=>{
+  getMeaning()
+})
+
+// This section handles the click event on the search icon for fetching the word's definition.
+searchText.addEventListener("keyup", () => {
+  getMeaning()
+});
+
+function getMeaning(){
+  // Get the trimmed text from the search input field
   let text = searchText.value.trim();
+
+  // Check if a valid text input is provided
   if (text !== "") {
+    // Display 'Loading...' while fetching the definition
     dictionaryDiv.innerHTML = "Loading...";
 
+    // Fetch the definition data from the Merriam-Webster's API
     fetch(
       `https://dictionaryapi.com/api/v3/references/learners/json/${text}?key=05a6f186-4232-4fd5-be99-0e12575a6a6d`
     )
       .then((response) => response.json())
       .then((data) => {
-        dictionaryDiv.innerHTML = ""; // Clear previous content
+        // Clear previous content from the dictionary div
+        dictionaryDiv.innerHTML = "";
 
+        // Check if the received data is an array and contains at least one result
         if (Array.isArray(data) && data.length > 0) {
           const wordData = data[0];
 
+          // Check if the 'shortdef' property exists for the word
           if (wordData.shortdef) {
-            // Display the first definition (you can loop through 'wordData.shortdef' for multiple definitions)
+            // Extract the part of speech and the first definition for the word
             const partOfSpeech = wordData.fl; // Part of speech
             const definition = wordData.shortdef[0]; // First definition
 
+            // Update the HTML to display the word, part of speech, and definition
             dictionaryDiv.innerHTML = `
             <div class="word-content">
-            <div class="text">
-              <h1>${text}</h1>
+              <div class="text">
+                <h1>${text}</h1>
+              </div>
+              <div class="play-logo">
+                <i class="fa fa-play" aria-hidden="true"></i>
+              </div>
             </div>
-            <div class="play-logo">
-              <i class="fa fa-play" aria-hidden="true"></i>
+            <div class="speech">
+              <p>${partOfSpeech}</p>
+              <hr />
             </div>
-          </div>
-          <div class="speech">
-            <p>${partOfSpeech}</p>
-            <hr />
-          </div>
-          <div class="meaning">Meaning</div>
-
-          <div class="meaning-text">
-            <li>${definition}</li>
-              `;
+            <div class="meaning">Meaning</div>
+            <div class="meaning-text">
+              <li>${definition}</li>
+            `;
           } else {
+            // If 'shortdef' is not available, display a message indicating no definition found
             dictionaryDiv.innerHTML = "No definition found for the word.";
           }
 
-          // Save the dictionary data to local storage
+          // Save the displayed dictionary data to local storage
           localStorage.setItem("lastDictionaryData", dictionaryDiv.innerHTML);
 
-          // Add event listener to the play icon button
+          // Add an event listener to the play icon button for text-to-speech
           const playIcon = dictionaryDiv.querySelector(".fa-play");
           playIcon.addEventListener("click", () => {
+            // Retrieve the definition text and initiate text-to-speech
             let definitionText =
               document.querySelector(".meaning-text li").innerHTML;
             speakDefinition(definitionText);
           });
         } else {
+          // If no data is found for the word, display an appropriate message
           dictionaryDiv.innerHTML = "No data found for the word.";
+          dictionaryDiv.classList.add("error");
         }
 
-        // Save the search term to local storage
+        // Save the searched term to local storage for future retrieval
         localStorage.setItem("lastSearchTerm", text);
       })
       .catch((error) => {
+        // Display an error message in case of API fetch failure
         dictionaryDiv.innerHTML = "Error: Unable to fetch data.";
+        dictionaryDiv.classList.add("error-text");
         console.error("Error:", error);
       });
 
-    searchText.value = "";
+    // Clear the search input field after the search is initiated
+    // searchText.value = "";
   }
-});
+}
 
 function speakDefinition(definitionText) {
   if ("speechSynthesis" in window) {
